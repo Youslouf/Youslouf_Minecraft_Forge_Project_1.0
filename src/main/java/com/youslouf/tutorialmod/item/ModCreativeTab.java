e1cb170abb50472efd76cbfa3c73916bd106f2d9
Youssef <youssef.misbah@outlook.com>
2022-06-24 22:10:51 +0200

    Video 6

--- src/main/java/com/youslouf/tutorialmod/item/ModCreativeTab.java
+++ src/main/java/com/youslouf/tutorialmod/item/ModCreativeTab.java
@@ -4,7 +4,7 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
 public class ModCreativeTab {
-    public static final CreativeModeTab TAB_FLUORITE = new CreativeModeTab("FLUORITE") {
+    public static final CreativeModeTab TAB_FLUORITE = new CreativeModeTab("fluorite") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(ModItems.FLUORITE.get());
