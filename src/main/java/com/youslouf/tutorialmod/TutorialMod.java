932276a4e7c29a969cce5de2ad27239b0c0bc37e
Youssef <youssef.misbah@outlook.com>
2022-06-24 20:21:51 +0200

    Video 4

--- src/main/java/com/youslouf/tutorialmod/TutorialMod.java
+++ src/main/java/com/youslouf/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package com.youslouf.tutorialmod;
 
+import com.youslouf.tutorialmod.block.ModBlocks;
 import com.youslouf.tutorialmod.item.ModItems;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -31,6 +32,7 @@ public class TutorialMod
 
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
         ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
         eventBus.addListener(this::setup);
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
---
+++ src/main/java/com/youslouf/tutorialmod/block/ModBlocks.java
@@ -0,0 +1,58 @@
+package com.youslouf.tutorialmod.block;
+
+import com.youslouf.tutorialmod.TutorialMod;
+import com.youslouf.tutorialmod.item.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+import java.util.function.ToIntFunction;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<Block> FLUORITE_BLOCK = registerBlock("fluorite_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
+                    .strength(5f).requiresCorrectToolForDrops().lightLevel((state) -> 15)
+            )
+            ,CreativeModeTab.TAB_BREWING);
+
+
+
+    private static <T extends Block> RegistryObject<T> registerBlock(
+            String name, Supplier<T> block, CreativeModeTab tab) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+
+        registerBlockItem(name, toReturn, tab);
+        return toReturn;
+
+    }
+
+
+
+
+
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
+                                                                            CreativeModeTab tab) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
+        new Item.Properties().tab(tab)));
+
+    }
+
+
+    public static void register(IEventBus eventBus){
+        BLOCKS.register(eventBus );
+    }
+
+}
