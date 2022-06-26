91f9789efa5f5a264c0f10fd7e04c02cf3b01df8
Youssef <youssef.misbah@outlook.com>
2022-06-26 23:58:28 +0200

    Video 10

---
+++ src/main/java/com/youslouf/tutorialmod/item/ModFoods.java
@@ -0,0 +1,8 @@
+package com.youslouf.tutorialmod.item;
+
+import net.minecraft.world.food.FoodProperties;
+
+public class ModFoods {
+    public static final FoodProperties FLUORITE_FLOSS = (new FoodProperties.Builder()).nutrition(5).fast().saturationMod(0.5f).build();
+
+}
--- src/main/java/com/youslouf/tutorialmod/item/ModItems.java
+++ src/main/java/com/youslouf/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package com.youslouf.tutorialmod.item;
 
 import com.youslouf.tutorialmod.TutorialMod;
+import com.youslouf.tutorialmod.item.custom.fluorite_dropper;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -15,6 +16,14 @@ public class ModItems {
     public static final RegistryObject<Item> FLUORITE = ITEMS.register("fluorite",
             () -> new Item(new Item.Properties().tab(ModCreativeTab.TAB_FLUORITE)));
 
+    public static final RegistryObject<Item> FLUORITE_DROPPER = ITEMS.register("fluorite_dropper",
+            () -> new fluorite_dropper(new Item.Properties().tab(ModCreativeTab.TAB_FLUORITE).durability(32)));
+
+    public static final RegistryObject<Item> FLUORITE_FLOSS = ITEMS.register("fluorite_floss",
+            () -> new Item(new Item.Properties().tab(ModCreativeTab.TAB_FLUORITE).food(ModFoods.FLUORITE_FLOSS)));
+
+    public static final RegistryObject<Item> FLUORITE_SUGAR = ITEMS.register("fluorite_sugar",
+            () -> new Item(new Item.Properties().tab(ModCreativeTab.TAB_FLUORITE)));
 
 
     public static void register(IEventBus eventBus) {
